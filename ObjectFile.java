62ccbf540b2d3ca39e3300d7103a23fc35e044dd
Abed <aislam360@gmail.com>
2019-12-19 04:44:37 -0500

    Added printlines

--- ObjectFile.java
+++ ObjectFile.java
@@ -3,3 +3,12 @@ Make a class that creates objects.
 Objects have to be read from file.
 Objects have to be written to file.
 */
+
+public class ObjectFile {
+	public static void main(String[] args){
+
+		System.out.println("Welcome to my Object Creater in java");
+		System.out.println("This program will read and write objects to and from a file");
+
+	}
+}
