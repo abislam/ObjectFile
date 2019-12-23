4533f8c7bd20782a9a0ce162bf35009977ab8461
Abed <aislam360@gmail.com>
2019-12-23 01:41:40 -0500

    updated the car class

--- ObjectFile.java
+++ ObjectFile.java
@@ -12,3 +12,19 @@ public class ObjectFile {
 
 	}
 }
+
+class Car{
+
+	private int id;
+	private String company;
+	private String model;
+	private int year;
+
+	public Car(int id, String company, String model, int year){
+		this.id = id;
+		this.company = company;
+		this.model = model;
+		this.year = year;
+	}
+
+}
