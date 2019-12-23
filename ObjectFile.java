9e99c46d84961ec7701fa5d352d68f3abca2f71e
Abed <aislam360@gmail.com>
2019-12-23 01:55:53 -0500

    Updated class car and main. Need to test with arrays

--- ObjectFile.java
+++ ObjectFile.java
@@ -10,6 +10,14 @@ public class ObjectFile {
 		System.out.println("Welcome to my Object Creater in java");
 		System.out.println("This program will read and write objects to and from a file");
 
+		Car sedan = new Car(0, "Mercedes", "S Class", 2020);
+		Car suv = new Car(1, "Dodge", "Durango", 2020);
+		Car coup = new Car(2, "Lamborghini", "Gallardo", 2020);
+
+
+		System.out.println(sedan.getID() + "\t" + sedan.getCompany() + "\t" + sedan.getModel() + "\t" + sedan.getYear());
+		System.out.println(suv.getID() + "\t" + suv.getCompany() + "\t" + suv.getModel() + "\t" + suv.getYear());
+		System.out.println(coup.getID() + "\t" + coup.getCompany() + "\t" + coup.getModel() + "\t" + coup.getYear());
 	}
 }
 
@@ -27,4 +35,70 @@ class Car{
 		this.year = year;
 	}
 
+	//getters
+	public int getID(){
+		return id;
+	}
+
+	public String getCompany(){
+		return company;
+	}
+
+	public String getModel(){
+		return model;
+	}
+
+	public int getYear(){
+		return year;
+	}
+
+	//setters
+	public void setID(int id){
+		this.id = id;
+	}
+
+	public void setCompany(String company){
+		this.company = company;
+	}
+
+	public void setModel(String model){
+		this.model = model;
+	}
+
+	public void setYear(int year){
+		this.year = year;
+	}
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
